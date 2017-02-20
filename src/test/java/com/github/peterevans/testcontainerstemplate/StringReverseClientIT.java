a1620ba5d83c93b2182574c813d32a1729bf5977
peter-evans <peter-evans@users.noreply.github.com>
2017-02-20 16:15:28 +0900

    Added string reverse container

--- src/test/java/com/github/peterevans/testcontainerstemplate/StringReverseClientIT.java
+++ src/test/java/com/github/peterevans/testcontainerstemplate/StringReverseClientIT.java
@@ -1,11 +1,20 @@
 package com.github.peterevans.testcontainerstemplate;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.images.builder.ImageFromDockerfile;
 
 /**
  * Integration tests for StringReverseClient.
  */
 public class StringReverseClientIT {
+    @Rule
+    public GenericContainer srContainer = new GenericContainer(
+            new ImageFromDockerfile()
+                    .withFileFromClasspath("nginx.conf", "string-reverse-server/nginx.conf")
+                    .withFileFromClasspath("Dockerfile", "string-reverse-server/Dockerfile"));
+
     @Test
     public void canReverseString() throws Exception {
         StringReverseClient srClient = new StringReverseClient("http://localhost:8080/");
