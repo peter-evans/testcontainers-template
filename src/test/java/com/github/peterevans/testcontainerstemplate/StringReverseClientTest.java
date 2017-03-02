b6fc51ac2ead0a89b8f37a44c1276e04e84b9300
peter-evans <peter-evans@users.noreply.github.com>
2017-03-02 14:48:57 +0900

    Added unit test for reverse method

--- src/test/java/com/github/peterevans/testcontainerstemplate/StringReverseClientTest.java
+++ src/test/java/com/github/peterevans/testcontainerstemplate/StringReverseClientTest.java
@@ -1,8 +1,12 @@
 package com.github.peterevans.testcontainerstemplate;
 
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -10,9 +14,27 @@ import static org.junit.Assert.assertNotNull;
  */
 @Category(UnitTest.class)
 public class StringReverseClientTest {
+    @Rule
+    public MockWebServer mockWebServer = new MockWebServer();
+
     @Test
     public void canCreateStringReverseClient() throws Exception {
         StringReverseClient srClient = new StringReverseClient("localhost", 8080);
         assertNotNull("An instance of StringReverseClient can be created.", srClient);
+    }
+
+    @Test
+    public void canReverseString() throws Exception {
+        String testStr = "reverse-me";
+        String expected = "em-esrever";
+
+        StringReverseClient srClient = new StringReverseClient(mockWebServer.getHostName(), mockWebServer.getPort());
+        mockWebServer.enqueue(new MockResponse()
+                .setResponseCode(200)
+                .setBody(expected));
+
+        assertEquals("A string can be reversed by calling the reverse method",
+                srClient.reverse(testStr),
+                expected);
     }
 }
